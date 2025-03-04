b588a0ec692b82528ce6af47e69de06f5856149c
Thiago <thiagolinog8@gmail.com>
2025-03-04 18:25:48 -0300

    Nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,72 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+struct CartasSuperTrunfo
+{
+	unsigned long int populacao;
+	float area;
+	float pib;
+	int pontos_turisticos;
+	int densidade_populacional;
+	int pib_per_capita;
+	float super_poder;
+};
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+void cadastrar_cartas(struct CartasSuperTrunfo cartas[], int quantidade_cartas)
+{
+	for (int i = 0; i < quantidade_cartas; i++)
+	{
+		printf("\nCarta %d\n", i + 1);
+		printf("Digite a população da cidade: ");
+		scanf("%d", &cartas[i].populacao);
+		printf("Digite a área da cidade: ");
+		scanf("%f", &cartas[i].area);
+		printf("Digite o PIB da cidade: ");
+		scanf("%f", &cartas[i].pib);
+		printf("Digite a quantidade de pontos turísticos da cidade: ");
+		scanf("%d", &cartas[i].pontos_turisticos);
+		cartas[i].densidade_populacional = cartas[i].populacao / cartas[i].area;
+		cartas[i].pib_per_capita = cartas[i].pib / cartas[i].populacao;
+		cartas[i].super_poder = cartas[i].populacao + cartas[i].area + cartas[i].pib + cartas[i].pontos_turisticos + (1 / cartas[i].densidade_populacional) + cartas[i].pib_per_capita;
+	};
+};
 
-    return 0;
-}
+void comparar_cartas(struct CartasSuperTrunfo cartas[], int quantidade_cartas)
+{
+	int carta1, carta2;
+	printf("\nEscolha as duas cartas que deseja comparar: ");
+	printf("\nCarta 1: ");
+	scanf("%d", &carta1);
+	printf("Carta 2: ");
+	scanf("%d", &carta2);
+	printf("\nA carta mais poderosa é a: %d\n", cartas[carta1 - 1].super_poder > cartas[carta2 - 1].super_poder ? carta1 : carta2);
+};
+
+void exibir_cartas(struct CartasSuperTrunfo cartas[], int quantidade_cartas)
+{
+	for (int i = 0; i < quantidade_cartas; i++)
+	{
+		printf("\nCarta %d\n", i + 1);
+		printf("População: %lu\n", cartas[i].populacao);
+		printf("Área: %.2f\n", cartas[i].area);
+		printf("PIB: %.2f\n", cartas[i].pib);
+		printf("Pontos Turísticos: %d\n", cartas[i].pontos_turisticos);
+		printf("Densidade Populacional: %.2f\n", cartas[i].densidade_populacional);
+		printf("PIB per Capita: %.2f\n", cartas[i].pib_per_capita);
+		printf("Super Poder: %.2f\n", cartas[i].super_poder);
+	};
+};
+
+int main()
+{
+	int quantidade_cartas;
+
+	printf("Digite a quantidade de cartas que deseja cadastrar: ");
+	scanf("%d", &quantidade_cartas);
+	struct CartasSuperTrunfo cartas[quantidade_cartas];
+
+	cadastrar_cartas(cartas, quantidade_cartas);
+	exibir_cartas(cartas, quantidade_cartas);
+	comparar_cartas(cartas, quantidade_cartas);
+
+	return 0;
+};
